2c9ddae71c9cfe6fa9fe850860a04ed36e1a0eb0
ridwanpranata <ridwanpranata10@gmail.com>
2023-04-17 07:58:06 +0700

    adjustment menu sidebar

--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -15,38 +15,29 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
             with font-awesome or any other icon font library -->
-                <li class="nav-header">EXAMPLES</li>
+                <li class="nav-header">Menu</li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <i class="nav-icon fas fa-server"></i>
                         <p>
-                            Dashboard
+                            Master Data
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="../../index.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v1</p>
-                            </a>
-                        </li>
+                    <ul class="ml-3 nav nav-treeview">
                         <li class="nav-item">
                             <a href="<?= url_to('gls') ?>" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Gls</p>
                             </a>
                         </li>
+                        <li class="nav-item">
+                            <a href="<?= url_to('lines') ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Lines</p>
+                            </a>
+                        </li>
                     </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="pages/calendar.html" class="nav-link">
-                        <i class="nav-icon far fa-calendar-alt"></i>
-                        <p>
-                            Calendar
-                            <span class="badge badge-info right">2</span>
-                        </p>
-                    </a>
                 </li>
                 
             </ul>
