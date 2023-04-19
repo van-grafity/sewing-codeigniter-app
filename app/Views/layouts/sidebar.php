4ca0dba97d29b0e51db754f0aaa4ccf4e14d8e58
ridwanpranata <ridwanpranata10@gmail.com>
2023-04-19 08:03:15 +0700

    change logo path in sidebar

--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="<?= base_url('adminLTE'); ?>/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+        <img src="<?= base_url('adminLTE'); ?>/img/AdminLTELogo.png" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Sewing App</span>
     </a>
