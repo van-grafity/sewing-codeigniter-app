134c2afe34dbcc78443c685099ee4a1d3887275b
ridwanpranata <ridwanpranata10@gmail.com>
2023-05-09 08:58:04 +0700

    add UsersSeeder

---
+++ app/Database/Seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    private $table = 'users';
+    public function run()
+    {
+        $data = [
+            [
+                'id' => 1,
+                'email' => 'admin@ghimli.com',
+                'password' => password_hash('123456789', PASSWORD_BCRYPT),
+                'name' => 'Admin IT',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 2,
+                'email' => 'sewing@ghimli.com',
+                'password' => password_hash('123456789', PASSWORD_BCRYPT),
+                'name' => 'Sewing Clerk',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        $this->db->table($this->table)->insertBatch($data);
+    }
+}
