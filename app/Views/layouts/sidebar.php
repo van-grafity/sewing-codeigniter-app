0f3839c59df92cc2e6622739b640df3cc2c74869
ridwanpranata <ridwanpranata10@gmail.com>
2023-05-10 08:29:24 +0700

    change home url sidebar

--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?= url_to('home') ?>" class="brand-link">
+    <a href="<?= url_to('/') ?>" class="brand-link">
         <img src="<?= base_url('adminLTE'); ?>/img/AdminLTELogo.png" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Sewing App</span>
@@ -17,7 +17,7 @@
             with font-awesome or any other icon font library -->
                 <li class="nav-header">Menu</li>
                 <li class="nav-item">
-                    <a href="<?= url_to('home') ?>" class="nav-link">
+                    <a href="<?= url_to('/') ?>" class="nav-link">
                         <i class="fas fa-home nav-icon"></i>
                         <p>Home</p>
                     </a>
