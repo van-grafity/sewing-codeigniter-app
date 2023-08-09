ef9e1d2d43121e14b25aff0d6cf4b41bc16c0bf5
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2023-08-09 10:05:22 +0700

    change title on landing page

--- app/Views/landing-page.php
+++ app/Views/landing-page.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Cutting Ticket App</title>
+        <title>Sewing App</title>
         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
         
         <!-- Font Awesome icons (free version)-->
